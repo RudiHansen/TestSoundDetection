a1ffa727b40a6cecb5a2dc6122eb134ae42bbc3c
RudiHansen <rsh@pobox.com>
2018-09-06 12:54:02 +0200

    Add project files.

---
+++ TestSoundDetection/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestSoundDetection.Classes;
+
+namespace TestSoundDetection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+                Fish().Wait();
+
+            //Console.WriteLine("Press key to continue!");
+            //Console.ReadKey();
+        }
+
+        public static async Task Fish()
+        {
+            // Inspiration for code from https://github.com/UltimateFishbot/UltimateFishbot
+            Ears ears = new Ears();
+
+            var fishHeard = await ears.Listen(2000, new System.Threading.CancellationToken());
+
+            Console.WriteLine($"{fishHeard}");
+
+        }
+
+    }
+}
